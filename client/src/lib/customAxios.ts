e549c6a3bd1e3c56e7690da95024633d0f027376
Shubham Vishwakarma <shubham.vish.2633@gmail.com>
2024-04-07 23:31:45 +0530

    fix types

--- client/src/lib/customAxios.ts
+++ client/src/lib/customAxios.ts
@@ -1,17 +1,21 @@
 import { toast } from "@/components/ui/use-toast";
 import axios from "axios";
 
-const customAxios = async (options) => {
+const customAxios = async (options: any) => {
   try {
     const response = await axios(options);
 
     return response.data.data;
   } catch (error) {
-    toast({
-      variant: "destructive",
-      title: "Error",
-      description: error?.response.data.error,
-    });
+    if (axios.isAxiosError(error)) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: error?.response?.data.error,
+      });
+    } else {
+      console.error(error);
+    }
   }
 };
 
